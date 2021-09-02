6c7f94197e9dae75213e5dd4a97e75989ca40e13
Carson-Key <keyc@oregonstate.edu>
2021-09-02 14:58:52 -0700

    Fetch Kaltura status page html

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 
 const App = () => {
 	const [boxStatus, setBoxStatus] = useState({status: {description: "loading..."}})
+	const [kalturaSatus, setKalturaSatus] = useState("loading...")
 
 	useEffect(() => {
 		fetch('https://status.box.com/api/v2/status.json')
@@ -10,12 +11,24 @@ const App = () => {
 			.then((json) => {
 				setBoxStatus(json)
 			})
+
+		fetch('https://status.kaltura.com')
+			.then((response) => {
+				return response.text()
+			})
+			.then((html) => {
+				let parser = new DOMParser()
+				console.log(parser.parseFromString(html, 'text/html'))
+			
+			})
 	}, [])
 
 	return (
 		<>
 			<p>Box Status:</p>
 			<p>{boxStatus.status.description}</p>
+			<p>Kaltura Status:</p>
+			<p>{kalturaSatus}</p>
 		</>
 	)
 }
