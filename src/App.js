084edd4e1a061cf7195b8f1d3853ddc09bdad62f
Carson-Key <keyc@oregonstate.edu>
2021-09-08 11:17:10 -0700

    Add Summary component to App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,15 @@
-// Packages
-import { useEffect, useState } from 'react'
+// Components
+import Summary from './components/Summary'
 // Helpers
-import { fetchJSON, apiLinks, apiInitSummaryState } from './helpers/statusAPICall.js'
+import { APIs } from './helpers/statusAPICall.js'
 
 const App = () => {
-	const [boxSummary, setBoxSummary] = useState(apiInitSummaryState)
-	const [kalturaSummary, setKalturaSummary] = useState(apiInitSummaryState)
-	const [instructureSummary, setInstructureSummary] = useState(apiInitSummaryState)
-	const [zoomSummary, setZoomSummary] = useState(apiInitSummaryState)
-
-	useEffect(() => {
-		fetchJSON(apiLinks.Box, setBoxSummary)
-		fetchJSON(apiLinks.Kaltura, setKalturaSummary)
-		fetchJSON(apiLinks.Instructure, setInstructureSummary)
-		fetchJSON(apiLinks.Zoom, setZoomSummary)
-	}, [])
-
 	return (
 		<>
-			<p>Box Status:</p>
-			<p>{boxSummary.status.description}</p>
-			{
-				boxSummary.incidents.map((item, i) => {
-					return (
-						<p key={i}>{item.name}</p>
-					)
-				})
-			}
-			<br />
-			<p>Kaltura Status:</p>
-			<p>{kalturaSummary.status.description}</p>
-			{
-				kalturaSummary.incidents.map((item, i) => {
-					return (
-						<p key={i}>{item.name}</p>
-					)
-				})
-			}
-			<br />
-			<p>Instructor Status:</p>
-			<p>{instructureSummary.status.description}</p>
-			{
-				instructureSummary.incidents.map((item, i) => {
-					return (
-						<p key={i}>{item.name}</p>
-					)
-				})
-			}
-			<br />
-			<p>Zoom Status:</p>
-			<p>{zoomSummary.status.description}</p>
-			{
-				zoomSummary.incidents.map((item, i) => {
-					return (
-						<p key={i}>{item.name}</p>
-					)
-				})
-			}
+			<Summary api={APIs.Box} />
+			<Summary api={APIs.Kaltura} />
+			<Summary api={APIs.Instructure} />
+			<Summary api={APIs.Zoom} />
 		</>
 	)
 }
