28af7870a8051aa412d65b6d079b6275dda69abe
Carson-Key <keyc@oregonstate.edu>
2021-09-08 10:12:51 -0700

    Add object of api links

--- src/helpers/statusAPICall.js
+++ src/helpers/statusAPICall.js
@@ -1,3 +1,10 @@
+export const apiLinks = {
+    Box: 'https://status.box.com/api/v2/status.json',
+    Kaltura: 'https://status.kaltura.com/api/v2/status.json',
+    Instructure: 'https://status.instructure.com/api/v2/status.json',
+    Zoom: 'https://status.zoom.us/api/v2/status.json'
+}
+
 export const fetchJSON = (url, setState) => {
     fetch(url)
         .then((response) => response.json())
