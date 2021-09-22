1f32f1c581445d621e823998f6d77d65733685f9
Carson-Key <keyc@oregonstate.edu>
2021-09-22 11:08:16 -0700

    Add testing helpers

---
+++ src/helpers/statusAPITest.js
@@ -0,0 +1,34 @@
+// Helpers
+import Active from './ExampleAPICalls/active.js'
+import { APIs, apiInitSummaryState as importAPIInitSummaryState } from './statusAPICall.js'
+
+export const apiInitSummaryState = {...importAPIInitSummaryState}
+
+export const fetchJSON = (url, setState) => {
+    switch (url) {
+        case APIs.Box.link:
+            testFetchJSON(setState, 0)
+            break;
+        case APIs.Kaltura.link:
+            testFetchJSON(setState, 0)
+            break;
+        case APIs.Instructure.link:
+            testFetchJSON(setState, 0)
+            break;
+        case APIs.Zoom.link:
+            testFetchJSON(setState, 0)
+            break;
+        default:
+            testFetchJSON(setState, 0)
+            break;
+    }
+
+}
+
+export const testFetchJSON = (setState, status) => {
+    setState(testStatus[status])
+}
+
+const testStatus = {
+    0: Active
+}
