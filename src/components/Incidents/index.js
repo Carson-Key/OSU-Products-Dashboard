8a06cbc4b94ebb63382c4e24708284cbb0151f80
Carson-Key <keyc@oregonstate.edu>
2021-09-30 13:07:49 -0700

    Add text css to  incidents

--- src/components/Incidents/index.js
+++ src/components/Incidents/index.js
@@ -2,13 +2,13 @@ const Incidents = (props) => {
     const { incidents } = props
 
 	return (
-        <ul>
+        <ul className="overflow-auto flex justify-center items-center h-52">
             {
                 incidents.map((incident, i) => {
                     return (
-                        <li key={i}>
-                            <p>{incident.name}</p>
-                            <p>{incident.impact}</p>
+                        <li key={i} className="px-3 pt-2">
+                            <center><p className="text-lg">Impact: {incident.impact}</p></center>
+                            <center><p className="mt-4">{incident.name}</p></center>
                         </li>
                     )
                 })
