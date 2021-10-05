f671f3127c510476c3766053d74e42c16dceb7e9
Carson-Key <keyc@oregonstate.edu>
2021-10-05 12:34:33 -0700

    Add anchor to AddApi card

--- src/components/AddNewStatus/index.js
+++ src/components/AddNewStatus/index.js
@@ -1,10 +1,14 @@
-import { MdAddCircleOutline } from "react-icons/md";
+// Packages
+import { MdAddCircleOutline } from "react-icons/md"
+import { Link } from "react-router-dom"
 
 const StatusNone = (props) => {
     return (
-        <div className="flex justify-center items-center h-60">
-            <MdAddCircleOutline className="text-gray-400 h-20 w-20" />
-        </div>
+        <Link to="/add">
+            <div className="flex justify-center items-center h-60">
+                <MdAddCircleOutline className="text-gray-400 h-20 w-20" />
+            </div>
+        </Link>
     )
 }
 
--- src/components/AddStatusHead/index.js
+++ src/components/AddStatusHead/index.js
@@ -1,10 +1,15 @@
+// Packages
+import { Link } from "react-router-dom"
+
 const AddStatusHead = (props) => {
 	const { name, color } = props
 
 	return (
-		<div className={color + " text-white px-8 py-2 rounded-t-lg"}>
-			<center><p>{name}</p></center>
-		</div>
+		<Link to="/add">
+			<div className={color + " text-white px-8 py-2 rounded-t-lg"}>
+				<center><p>{name}</p></center>
+			</div>
+		</Link>
 	)
 }
 
