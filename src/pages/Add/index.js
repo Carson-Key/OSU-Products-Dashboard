2277dd3266f46423ba3096aef5f4ffc2c1750856
Carson-Key <keyc@oregonstate.edu>
2021-10-05 13:58:00 -0700

    Add cookies to track enabled cards

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,15 +1,20 @@
 // Package
 import { useState } from 'react'
+import { useCookies } from 'react-cookie';
 // Helpers
 import { APIs, AddURL } from '../../helpers/statusAPIObjects.js'
 import { defaultObjectValue } from '../../helpers/basic.js'
+import { useEffect } from 'react';
 
 const Add = () => {
-    const [enabledCards, setEnabledCards] = useState({})
+    const [apiCookie, setApiCookie] = useCookies(['APIs'])
+    const [enabledCards, setEnabledCards] = useState({...apiCookie.APIs})
     const defaultAPISArray = Object.keys(APIs)
 
-    const toggleStatusAPI = (event, enabledCards, api) => {
+    const toggleStatusAPI = (event, api) => {
         let newStateValue = {...enabledCards}
+
+        console.log(newStateValue)
 
         if (newStateValue[api]) {
             delete newStateValue[api]
@@ -19,6 +24,13 @@ const Add = () => {
         
         setEnabledCards(newStateValue)
     }
+    const saveNewConfig = () => {
+        setApiCookie('APIs', enabledCards, { path: '/' })
+    }
+
+    useEffect(() => {
+        console.log(enabledCards)
+    }, [])
 
 	return (
         <div className="flex flex-wrap justify-evenly">
@@ -35,13 +47,14 @@ const Add = () => {
                                         name={api}
                                         type="checkbox"
                                         checked={defaultObjectValue(enabledCards, api)}
-                                        onChange={(event) => {toggleStatusAPI(event, enabledCards, api)}}
+                                        onChange={(event) => {toggleStatusAPI(event, api)}}
                                     />
                                 </label>
                             )
                         }
                     })
                 }
+                <button className="border-2 px-2" onClick={saveNewConfig}>Save</button>
             </form>
         </div>
 	)
