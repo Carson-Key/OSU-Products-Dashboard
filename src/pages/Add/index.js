420c82a6e91d4dd9a9dc08bcb9e7988a6b0aee34
Carson-Key <keyc@oregonstate.edu>
2021-10-05 12:55:53 -0700

    Add dynamic generation of default apis

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,7 +1,30 @@
+// Helpers
+import { APIs, AddURL } from '../../helpers/statusAPIObjects.js'
+
 const Add = () => {
+    const defaultAPISArray = Object.keys(APIs)
+
 	return (
         <div className="flex flex-wrap justify-evenly">
-            add
+            <form className="w-2/3 flex flex-wrap justify-evenly">
+                {
+                    defaultAPISArray.map((api, i) => {
+                        if (APIs[api].link === AddURL) {
+                            return <></>
+                        } else {
+                            return (
+                                <label className="mx-4">
+                                    {api}
+                                    <input
+                                        name={api}
+                                        type="checkbox"
+                                    />
+                                </label>
+                            )
+                        }
+                    })
+                }
+            </form>
         </div>
 	)
 }
