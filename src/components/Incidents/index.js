c2d1cabca885e094f2a6d255290a7ffae78dead5
Carson-Key <keyc@oregonstate.edu>
2021-10-05 10:41:38 -0700

    Fix css with multiple incidents

--- src/components/Incidents/index.js
+++ src/components/Incidents/index.js
@@ -2,11 +2,11 @@ const Incidents = (props) => {
     const { incidents } = props
 
 	return (
-        <ul className="overflow-auto flex justify-center items-center h-52">
+        <ul className="overflow-auto flex flex-wrap justify-center items-center h-52 divide-y-2 divide-solid">
             {
                 incidents.map((incident, i) => {
                     return (
-                        <li key={i} className="px-3 pt-2">
+                        <li key={i} className="p-3 py-5 w-full">
                             <center><p className="text-lg">Impact: {incident.impact}</p></center>
                             <center><p className="mt-4">{incident.name}</p></center>
                         </li>
