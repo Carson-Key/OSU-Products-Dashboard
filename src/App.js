387d7f1c41c3b76ac1f9de99ba11784537bd4dc6
Carson-Key <keyc@oregonstate.edu>
2021-10-05 12:26:31 -0700

    Add router routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+// Packages
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+  } from "react-router-dom";
 // Components
 import Dashboard from './pages/Dashboard'
 
 const App = () => {
 	return (
-		<>
+		<Router>
 			<header className="flex w-screen h-16 my-auto bg-osuOrange">
 				<p className="text-white mx-auto my-auto lg:ml-6 text-3xl">
 					Products Status
 				</p>
 			</header>
 			<main className="h-screen my-10">
-				<Dashboard />
+				<Switch>
+					<Route path="/">
+						<Dashboard />
+					</Route>
+				</Switch>
 			</main>
-		</>
+		</Router>
 	)
 }
 
