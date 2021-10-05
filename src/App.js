d7bc7a475df6fe80cd849bedd23b5a7d8df7d1dd
Carson-Key <keyc@oregonstate.edu>
2021-10-05 10:49:24 -0700

    Fix bottom margin on mobile

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import { APIs } from './helpers/statusAPICall.js'
 
 const App = () => {
 	return (
-		<div>
+		<>
 			<header className="flex w-screen h-16 my-auto bg-osuOrange">
 				<p className="text-white mx-auto my-auto lg:ml-6 text-3xl">
 					Products Status
 				</p>
 			</header>
-			<main className="flex flex-wrap justify-evenly py-10 h-screen">
-				<Summary api={APIs.Box} />
-				<Summary api={APIs.Kaltura} />
-				<Summary api={APIs.Instructure} />
-				<Summary api={APIs.Zoom} />
+			<main className="h-screen my-10">
+				<div className="flex flex-wrap justify-evenly">
+					<Summary api={APIs.Box} />
+					<Summary api={APIs.Kaltura} />
+					<Summary api={APIs.Instructure} />
+					<Summary api={APIs.Zoom} />
+				</div>
 			</main>
-		</div>
+		</>
 	)
 }
 
