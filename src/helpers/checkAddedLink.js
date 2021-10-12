92a41f63cea8c664a4c30614cfcd1aace809aa58
Carson-Key <keyc@oregonstate.edu>
2021-10-12 13:50:32 -0700

    Extract checking if addedAPIs is a cookie

--- src/helpers/checkAddedLink.js
+++ src/helpers/checkAddedLink.js
@@ -9,25 +9,48 @@ const checkIfLinkIsLive = (apiLink, onSuccess) => {
     });
 }
 
+const checkIfAddedAPICookieExsists = (apiCookie, onSuccess, onFail) => {
+    if (apiCookie.addedAPIs) {
+        onSuccess()
+    } else {
+        onFail()
+    }
+}
+
 export const addNewAPI = (event, apiName, apiLink, apiCookie, setApiCookie) => {
     event.preventDefault()
     const beatifiedName = capitalizeFirstLetter(apiName.toLowerCase())
-    checkIfLinkIsLive(apiLink, () => {
-        if (apiCookie.addedAPIs) {
-            setApiCookie('AddedAPIs', {
-                ...apiCookie.addedAPIs, 
-                [beatifiedName]: {
-                    name: beatifiedName,
-                    link: apiLink
-                }
-            }, { path: '/' })
-        } else {
-            setApiCookie('AddedAPIs', {
-                [beatifiedName]: {
-                    name: beatifiedName,
-                    link: apiLink
-                }
-            }, { path: '/' })
+
+    const onComplete = {
+        checkIfLinkIsLive: {
+            onSuccess: () => {
+                checkIfAddedAPICookieExsists(
+                    apiCookie, 
+                    onComplete.checkIfAddedAPICookieExsists.onSuccess,
+                    onComplete.checkIfAddedAPICookieExsists.onFail
+                )
+            }
+        },
+        checkIfAddedAPICookieExsists: {
+            onSuccess: () => {
+                setApiCookie('AddedAPIs', {
+                    ...apiCookie.addedAPIs, 
+                    [beatifiedName]: {
+                        name: beatifiedName,
+                        link: apiLink
+                    }
+                }, { path: '/' })
+            },
+            onFail: () => {
+                setApiCookie('AddedAPIs', {
+                    [beatifiedName]: {
+                        name: beatifiedName,
+                        link: apiLink
+                    }
+                }, { path: '/' })
+            }
         }
-    })
+    }
+
+    checkIfLinkIsLive(apiLink, onComplete.checkIfLinkIsLive.onSuccess)
 }
