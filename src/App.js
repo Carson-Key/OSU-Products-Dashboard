57c24cce9deca924b354f5f73465ee81ff513968
Carson-Key <keyc@oregonstate.edu>
2021-10-12 10:58:14 -0700

    Extract Routes to own component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 // Packages
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route
-  } from "react-router-dom";
+	BrowserRouter as Router
+  } from "react-router-dom"
 // Components
-import Dashboard from './pages/Dashboard'
-import Add from './pages/Add'
 import Header from './components/Header'
+import PageRoutes from './components/PageRoutes'
 
 const App = () => {
 	return (
 		<Router>
 			<Header />
 			<main className="my-10">
-				<Switch>
-					<Route path="/add">
-						<Add />
-					</Route>
-					<Route path="/">
-						<Dashboard />
-					</Route>
-				</Switch>
+				<PageRoutes />
 			</main>
 		</Router>
 	)
---
+++ src/components/PageRoutes/index.js
@@ -0,0 +1,23 @@
+// Packages
+import {
+	Switch,
+	Route
+  } from "react-router-dom"
+// Components
+import Dashboard from '../../pages/Dashboard'
+import Add from '../../pages/Add'
+
+const PageRoutes = () => {
+	return (
+        <Switch>
+            <Route path="/add">
+                <Add />
+            </Route>
+            <Route path="/">
+                <Dashboard />
+            </Route>
+        </Switch>
+	)
+}
+
+export default PageRoutes
