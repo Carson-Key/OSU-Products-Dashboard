0865c217e2f80a4df3f57ef95b9120505814681d
Carson-Key <keyc@oregonstate.edu>
2021-10-12 10:53:32 -0700

    Extract Header to own component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,15 @@ import {
 	Switch,
 	Route
   } from "react-router-dom";
-import { NavLink } from "react-router-dom"
-import { MdSettings } from "react-icons/md";
 // Components
 import Dashboard from './pages/Dashboard'
 import Add from './pages/Add'
+import Header from './components/Header'
 
 const App = () => {
 	return (
 		<Router>
-			<header className="flex justify-between w-screen h-16 my-auto bg-osuOrange">
-				<NavLink to="/" className="text-white mx-auto my-auto lg:ml-6 text-3xl">
-					<h1>Products Status</h1>
-				</NavLink>
-				<NavLink to="/add" className="flex justify-center items-center h-full w-16">
-					<MdSettings className="text-white text-3xl" />
-				</NavLink>
-			</header>
+			<Header />
 			<main className="my-10">
 				<Switch>
 					<Route path="/add">
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+// Packages
+import { NavLink } from "react-router-dom"
+import { MdSettings } from "react-icons/md"
+
+const Header = () => {
+	return (
+        <header className="flex justify-between w-screen h-16 my-auto bg-osuOrange">    
+            <NavLink to="/" className="text-white mx-auto my-auto lg:ml-6 text-3xl">
+                <h1>Products Status</h1>
+            </NavLink>
+            <NavLink to="/add" className="flex justify-center items-center h-full w-16">
+                <MdSettings className="text-white text-3xl" />
+            </NavLink>
+        </header>
+	)
+}
+
+export default Header
