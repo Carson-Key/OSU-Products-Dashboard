6efb9882641f230722659d7662f4fad1642bf3fb
Carson-Key <keyc@oregonstate.edu>
2021-10-12 10:48:56 -0700

    Add settings Link to header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
 	Switch,
 	Route
   } from "react-router-dom";
+import { NavLink } from "react-router-dom"
+import { MdSettings } from "react-icons/md";
 // Components
 import Dashboard from './pages/Dashboard'
 import Add from './pages/Add'
@@ -11,10 +13,13 @@ import Add from './pages/Add'
 const App = () => {
 	return (
 		<Router>
-			<header className="flex w-screen h-16 my-auto bg-osuOrange">
+			<header className="flex justify-between w-screen h-16 my-auto bg-osuOrange">
 				<p className="text-white mx-auto my-auto lg:ml-6 text-3xl">
 					Products Status
 				</p>
+				<NavLink to="/add" className="flex justify-center items-center h-full w-16">
+					<MdSettings className="text-white text-3xl" />
+				</NavLink>
 			</header>
 			<main className="my-10">
 				<Switch>
