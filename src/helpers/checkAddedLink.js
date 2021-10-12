339f7022c55d6440941e33c6b595d42377ba92c7
Carson-Key <keyc@oregonstate.edu>
2021-10-12 13:38:29 -0700

    Extract checking live link

--- src/helpers/checkAddedLink.js
+++ src/helpers/checkAddedLink.js
@@ -1,28 +1,33 @@
 import { capitalizeFirstLetter } from './basic.js'
 
+const checkIfLinkIsLive = (apiLink, onSuccess) => {
+    return fetch(apiLink).then(() => {
+        onSuccess()
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
 export const addNewAPI = (event, apiName, apiLink, apiCookie, setApiCookie) => {
     event.preventDefault()
     const beatifiedName = capitalizeFirstLetter(apiName.toLowerCase())
-    fetch(apiLink)
-        .then((response) => {
-            if (apiCookie.addedAPIs) {
-                setApiCookie('AddedAPIs', {
-                    ...apiCookie.addedAPIs, 
-                    [beatifiedName]: {
-                        name: beatifiedName,
-                        link: apiLink
-                    }
-                }, { path: '/' })
-            } else {
-                setApiCookie('AddedAPIs', {
-                    [beatifiedName]: {
-                        name: beatifiedName,
-                        link: apiLink
-                    }
-                }, { path: '/' })
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    checkIfLinkIsLive(apiLink, () => {
+        if (apiCookie.addedAPIs) {
+            setApiCookie('AddedAPIs', {
+                ...apiCookie.addedAPIs, 
+                [beatifiedName]: {
+                    name: beatifiedName,
+                    link: apiLink
+                }
+            }, { path: '/' })
+        } else {
+            setApiCookie('AddedAPIs', {
+                [beatifiedName]: {
+                    name: beatifiedName,
+                    link: apiLink
+                }
+            }, { path: '/' })
+        }
+    })
 }
