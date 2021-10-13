6a4b0c48529e91894b5f260749257500b01edc5a
Carson-Key <keyc@oregonstate.edu>
2021-10-13 14:08:50 -0700

    Fix issue where user  unable to click nav links when message present

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,10 +5,10 @@ import { MdSettings } from 'react-icons/md'
 const Header = () => {
 	return (
         <header className="flex justify-between w-screen h-16 my-auto bg-osuOrange">    
-            <NavLink to="/" className="text-white mx-auto my-auto lg:ml-6 text-3xl">
+            <NavLink to="/" className="z-10 text-white mx-auto my-auto lg:ml-6 text-3xl">
                 <h1>Products Status</h1>
             </NavLink>
-            <NavLink to="/settings" className="flex justify-center items-center h-full w-16">
+            <NavLink to="/settings" className="z-10 flex justify-center items-center h-full w-16">
                 <MdSettings className="text-white text-3xl" />
             </NavLink>
         </header>
