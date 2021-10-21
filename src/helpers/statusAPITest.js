91d50e79dd7d23f3dd4e9d6cdb30d9b1dd5e36d4
Carson-Key <keyc@oregonstate.edu>
2021-10-21 15:45:31 -0700

    Remove branching paths in fetchJSON

--- src/helpers/statusAPITest.js
+++ src/helpers/statusAPITest.js
@@ -1,35 +1,20 @@
+// Helpers
+import { defaultKeyValue } from './basic.js'
 // Objects
 import { APIs, testStatus } from './statusAPIObjects.js'
 
 const defaultTestSate = {
-    [APIs.Box.name]: 0,
-    [APIs.Kaltura.name]: 0,
-    [APIs.Instructure.name]: 0,
-    [APIs.Zoom.name]: 0,
+    [APIs.Box.link]: 0,
+    [APIs.Kaltura.link]: 0,
+    [APIs.Instructure.link]: 0,
+    [APIs.Zoom.link]: 0,
     other: 0,
 }
 
-export const fetchJSON = (url, setState, testState = defaultTestSate) => {
-    switch (url) {
-        case APIs.Box.link:
-            testFetchJSON(setState, testState[APIs.Box.name])
-            break;
-        case APIs.Kaltura.link:
-            testFetchJSON(setState, testState[APIs.Kaltura.name])
-            break;
-        case APIs.Instructure.link:
-            testFetchJSON(setState, testState[APIs.Instructure.name])
-            break;
-        case APIs.Zoom.link:
-            testFetchJSON(setState, testState[APIs.Zoom.name])
-            break;
-        default:
-            testFetchJSON(setState, testState.other)
-            break;
-    }
-
+const testFetchJSON = (setState, status) => {
+    setState(testStatus[status])
 }
 
-export const testFetchJSON = (setState, status) => {
-    setState(testStatus[status])
+export const fetchJSON = (url, setState, testState = defaultTestSate) => {
+    testFetchJSON(setState, defaultKeyValue(url, testState))
 }
