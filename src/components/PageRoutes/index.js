561006838dca7ac90082f950f9b32f7126b58cf0
Carson-Key <keyc@oregonstate.edu>
2021-10-21 12:56:46 -0700

    Add debug links to reset cookies

--- src/components/PageRoutes/index.js
+++ src/components/PageRoutes/index.js
@@ -3,10 +3,12 @@ import {
 	Switch,
 	Route
 } from 'react-router-dom'
-// Components
+// Pages
 import Dashboard from '../../pages/Dashboard'
 import Add from '../../pages/Add'
 import Settings from '../../pages/Settings'
+import ResetActiveAPIs from '../../pages/ResetActiveAPIs'
+import ResetAddedAPIs from '../../pages/ResetAddedAPIs'
 
 const PageRoutes = () => {
 	return (
@@ -16,6 +18,16 @@ const PageRoutes = () => {
             </Route>
             <Route path="/settings">
                 <Settings />
+            </Route>
+            <Route path="/reset_active_apis">
+                <ResetActiveAPIs />
+            </Route>
+            <Route path="/reset_added_apis">
+                <ResetAddedAPIs />
+            </Route>
+            <Route path="/reset_all_api_cookies">
+                <ResetActiveAPIs />
+                <ResetAddedAPIs />
             </Route>
             <Route path="/">
                 <Dashboard />
---
+++ src/pages/ResetAddedAPIs/index.js
@@ -0,0 +1,24 @@
+// Packages
+import { useEffect } from 'react'
+import { useCookies } from 'react-cookie'
+import { useHistory } from 'react-router-dom'
+
+const ResetActiveAPIs = () => {
+    let history = useHistory()
+    const [apiCookie, setApiCookie, removeCookie] = useCookies(['APIs'])
+
+    // To please the compiler
+    if (apiCookie) {}
+    if (setApiCookie) {}
+    
+    useEffect(() => {
+        removeCookie('addedAPIs')
+        history.push("/")
+    }, [history, removeCookie])
+
+	return (
+        <></>
+	)
+}
+
+export default ResetActiveAPIs
