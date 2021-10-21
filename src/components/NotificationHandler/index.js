65d7facd0e5db0004a154dadeac9bf5919266fe1
Carson-Key <keyc@oregonstate.edu>
2021-10-21 13:35:56 -0700

    Beautify

--- src/components/NotificationHandler/index.js
+++ src/components/NotificationHandler/index.js
@@ -6,21 +6,21 @@ import Notification from '../Notification'
 import { NotificationContext } from '../../helpers/notificationHandling/NotificationContext.js'
 
 const NoificationHandler = () => {
-  const [state, dispatch] = useContext(NotificationContext)
+	const [state, dispatch] = useContext(NotificationContext)
 
-  // To satisfy the compiler warnings
-  if (dispatch) {}
+	// To satisfy the compiler warnings
+	if (dispatch) {}
 
-  switch (state.notification.occurs) {
-    case true:
-      return (
-        <Notification message={state.notification.message} type={state.notification.type} />
-      )
-    default:
-      return (
-        <Fragment></Fragment>
-      )
-  }
+	switch (state.notification.occurs) {
+		case true:
+			return (
+				<Notification message={state.notification.message} type={state.notification.type} />
+			)
+		default:
+			return (
+				<Fragment></Fragment>
+			)
+	}
 }
 
-export default NoificationHandler;
+export default NoificationHandler
