ac85769bf1f5d05d55ab007b9cc04f843690e488
Carson-Key <keyc@oregonstate.edu>
2021-10-21 13:39:11 -0700

    Beautify

--- src/components/StatusNone/index.js
+++ src/components/StatusNone/index.js
@@ -1,7 +1,7 @@
 // Packages
 import { FaRegCheckCircle } from 'react-icons/fa'
 
-const StatusNone = (props) => {
+const StatusNone = () => {
     return (
         <div className="flex justify-center items-center h-52">
             <FaRegCheckCircle className="text-green-400 h-20 w-20" />
