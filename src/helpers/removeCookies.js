39c566d69f6712a02897eef092914d4f2359b107
Carson-Key <keyc@oregonstate.edu>
2021-10-22 11:06:20 -0700

    Beautify

--- src/helpers/removeCookies.js
+++ src/helpers/removeCookies.js
@@ -5,18 +5,18 @@ export const removeActiveCookies = (removeCookie) => {
 
 export const removeAddedCookies = (apiCookie, setApiCookie, removeCookie) => {
     let newActiveAPIs = {}
-        let activeAPIsArray = []
+    let activeAPIsArray = []
 
-        if (apiCookie.APIs) {
-            newActiveAPIs = {...apiCookie.APIs}
-        }
-        if (apiCookie.addedAPIs) {
-            activeAPIsArray = Object.keys(apiCookie.addedAPIs)
-        }
-        
-        activeAPIsArray.forEach((api) => {
-            delete newActiveAPIs[api]
-        })
-        setApiCookie('APIs', newActiveAPIs, { path: '/' })
-        removeCookie('addedAPIs')
+    if (apiCookie.APIs) {
+        newActiveAPIs = {...apiCookie.APIs}
+    }
+    if (apiCookie.addedAPIs) {
+        activeAPIsArray = Object.keys(apiCookie.addedAPIs)
+    }
+    
+    activeAPIsArray.forEach((api) => {
+        delete newActiveAPIs[api]
+    })
+    setApiCookie('APIs', newActiveAPIs, { path: '/' })
+    removeCookie('addedAPIs')
 }
