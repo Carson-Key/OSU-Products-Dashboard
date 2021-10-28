4c682e53c1cc78f0f8f116c96eaa1cd3a23a9bb8
Carson-Key <keyc@oregonstate.edu>
2021-10-28 13:49:05 -0700

    Add popup to remove addedApi

--- src/UI/RemoveAddedAPI.js
+++ src/UI/RemoveAddedAPI.js
@@ -1,16 +1,34 @@
 // Package
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { useCookies } from 'react-cookie'
 // Helpers
 import { deleteAdded } from '../helpers/addActive'
+import { activatePopUp, deactivatePopUp } from '../helpers/popUpHandling/popUpHelpers'
+// Contexts
+import { PopUpContext } from '../helpers/popUpHandling/PopUpContext'
 
 const RemoveAddedAPI = (props) => {
     const { addedAPISArray } = props
     const [apiCookie, setApiCookie] = useCookies(['APIs'])
     const [enabledCards, setEnabledCards] = useState({...apiCookie.APIs})
+    const [popUpState, popUpDispatch] = useContext(PopUpContext)
+
+    // To satisfy the compiler warnings
+    if (popUpState) {}
 
     const deleteAddedAPI = (api) => {
-        deleteAdded(apiCookie, api, setApiCookie, enabledCards, setEnabledCards)
+        activatePopUp((
+            <>
+                <p>Are you sure that you wish to remove this status card?</p>
+                <div className="flex justify-center gap-x-2 my-5">
+                <button onClick={() => {
+                    deleteAdded(apiCookie, api, setApiCookie, enabledCards, setEnabledCards)
+                    deactivatePopUp(popUpDispatch)
+                }} className="bg-red-500 text-white px-2 py-1">Yes</button>
+                <button onClick={() => {deactivatePopUp(popUpDispatch)}} className="bg-gray-500 text-white px-2 py-1">No</button>
+                </div>
+            </>
+        ), popUpDispatch)
     }
 
 	return (
@@ -20,7 +38,10 @@ const RemoveAddedAPI = (props) => {
                     return (
                         <label key={i} className="grid grid-cols-1 w-24 font-semibold px-2 py-1">
                             {api}
-                            <button className="text-white bg-red-500" onClick={() => {deleteAddedAPI(api)}}>Remove</button>
+                            <button className="text-white bg-red-500" onClick={(event) => {
+                                event.preventDefault()
+                                deleteAddedAPI(api)
+                            }}>Remove</button>
                         </label>
                     )
                 })
--- src/helpers/popUpHandling/popUpHelpers.js
+++ src/helpers/popUpHandling/popUpHelpers.js
@@ -1,3 +1,12 @@
+export const deactivatePopUp = (dispatch) => {
+    dispatch({
+        type: 'SET_POPUP', 
+        payload: {
+            occurs: false, 
+            children: (<></>)
+        }
+    })
+}
 export const activatePopUp = (children, dispatch) => {
     dispatch({
         type: 'SET_POPUP', 
